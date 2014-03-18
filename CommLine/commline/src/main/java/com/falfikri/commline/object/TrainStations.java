aee9686231fc10f1c3298607e074594a270707ee
Zakiy <zakiy@icehousecorp.com>
2014-03-18 12:27:18 +0700

    Create TrainStation objects

---
+++ CommLine/commline/src/main/java/com/falfikri/commline/object/TrainStations.java
@@ -0,0 +1,16 @@
+package com.falfikri.commline.object;
+
+import android.location.Location;
+
+import com.falfikri.commline.model.TrainStation;
+
+/**
+ * Created by zakiy on 3/17/14.
+ */
+public class TrainStations {
+    public static final TrainStation STASIUN_BOGOR = new TrainStation("Stasiun Bogor",-6.594591, 106.790589);
+    public static final TrainStation STASIUN_CILEBUT = new TrainStation("Stasiun Cilebut",-6.530752, 106.800628);
+    public static final TrainStation STASIUN_BOJONGGEDE = new TrainStation("Stasiun Bojonggede",-6.594591, 106.790589);
+    public static final TrainStation STASIUN_CITAYAM = new TrainStation("Stasiun Citayam",-6.594591, 106.790589);
+
+}
