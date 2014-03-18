90a4bee967d97c6fc6f5f60eb266a9d13b37ce52
Zakiy <zakiy@icehousecorp.com>
2014-03-18 12:27:53 +0700

    Refactored TrainStation

---
+++ CommLine/commline/src/main/java/com/falfikri/commline/model/TrainStation.java
@@ -0,0 +1,39 @@
+package com.falfikri.commline.model;
+
+import android.location.Location;
+
+import com.falfikri.commline.util.LocationUtility;
+
+/**
+ * Created by zakiy on 3/17/14.
+ */
+public class TrainStation {
+    private String mName;
+    private Location mLocation;
+
+    public TrainStation(String name, Location location){
+        mName = name;
+        mLocation = location;
+    }
+
+    public TrainStation(String name, double latitude, double longitude){
+        mName = name;
+        mLocation = LocationUtility.toLocation(latitude,longitude);
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public Location getLocation() {
+        return mLocation;
+    }
+
+    public void setLocation(Location mLocation) {
+        this.mLocation = mLocation;
+    }
+}
